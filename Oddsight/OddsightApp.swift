4175b82418599e34171ed3b3a3b28b4c65076729
Emanuil Vartanyan <mike.vartanyan@yahoo.com>
2026-08-29 16:57:59 -0700

    Initial Commit

---
+++ Oddsight/OddsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddsightApp.swift
+//  Oddsight
+//
+//  Created by Emanuil Vartanyan on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct OddsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
